8934dac56ddb1f508857d9beb40a6f358cb25687
gosiazagorska <mzagorska4@st.swps.edu.pl>
2025-12-09 13:21:27 +0100

    dodawanie torebek wstęp

--- nasz_projekt/biblioteka/admin.py
+++ nasz_projekt/biblioteka/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Torebka
 
-# Register your models here.
+admin.site.register(Torebka)
--- nasz_projekt/biblioteka/models.py
+++ nasz_projekt/biblioteka/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+#dodawanie torebek 
+
+ROZM = models.IntegerChoices(
+    'ROZM',
+    'mini midi maxi'
+)
+
+
+class Torebka(models.Model):
+    
+    nazwa_modelu = models.CharField(max_length= 100, null = False,  blank = False )
+    marka =  models.CharField(max_length= 50, null = False,  blank = False )
+    rozmiar = models.IntegerField(choices = ROZM.choices, default= ROZM.choices[2][0])
+  
